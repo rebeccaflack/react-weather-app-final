5f360c1805da6011e9279a36c3723a6b27572e0c
rebeccaflack <68822249+rebeccaflack@users.noreply.github.com>
2021-01-22 23:16:33 -0500

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Rebecca Flack and is{" "}
-        <a
-          href="https://github.com/rebeccaflack/react-weather-app-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Rebecca Flack and is{" "}
+          <a
+            href="https://github.com/rebeccaflack/react-weather-app-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
