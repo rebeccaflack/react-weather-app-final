095560662ce6c146c511ca18d2934b5da00c8b2e
rebeccaflack <68822249+rebeccaflack@users.noreply.github.com>
2021-01-22 22:58:18 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Rebecca Flack and is{" "}
+        <a
+          href="https://github.com/rebeccaflack/react-weather-app-final"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
